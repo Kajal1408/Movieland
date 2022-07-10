166b214c4a3c8067e66fe36dd42fe8d36f7ef0dd
Kajal Chauhan <91744564+Kajal1408@users.noreply.github.com>
2022-07-10 11:05:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,6 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=73343210';
 
-const movie1={
-    
-    "Title": "Spiderman in Cannes",
-    "Year": "2016",
-    "imdbID": "tt5978586",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
-
-}
 const App = () => {
     const [movies, setMovies]= useState([]);
     const [searchTerm ,setSearchTerm] =useState('');
